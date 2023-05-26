66cc67e3a4d7f6099e75aba57aa4f9e591a05e84
dikshant-honda <dikshant_s@jp.honda>
2023-05-26 18:20:28 +0900

    background extraction automated

--- code/tests/test_background.py
+++ code/tests/test_background.py
@@ -2,11 +2,7 @@ import os
 import cv2
 import numpy as np
 
-R = 114
 ESC_KEY = 27
-SPACE = 32
-
-# -------------------------------------- Background Extraction -----------------------------------------------------
 
 def checkReduction(image):
     reduction = 1
@@ -29,10 +25,10 @@ def calcBackgound(VideoPath, reduc, Save=None):
     while True:
         ret, f = cap.read()
         if not ret: break
-        cv2.imshow('Main Video', cv2.resize(f, (f.shape[1]// reduc , f.shape[0] // reduc)))
+        # cv2.imshow('Main Video', cv2.resize(f, (f.shape[1]// reduc , f.shape[0] // reduc)))
         cv2.accumulateWeighted(f, img_bkgd, 0.01)
         res2 = cv2.convertScaleAbs(img_bkgd)
-        cv2.imshow('When you feel the background is good enough, press ESC to terminate and save the background.', cv2.resize(res2, (res2.shape[1]// reduc , res2.shape[0] // reduc)))
+        # cv2.imshow('When you feel the background is good enough, press ESC to terminate and save the background.', cv2.resize(res2, (res2.shape[1]// reduc , res2.shape[0] // reduc)))
         k = cv2.waitKey(20)
         if k == 27: break
     if Save: cv2.imwrite(Save, res2)
@@ -41,5 +37,9 @@ def calcBackgound(VideoPath, reduc, Save=None):
     return res2
 
 if __name__ == "__main__":
-    video_path = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/output.mp4"
-    calcBackgound(video_path, 1, "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/background.jpg")
+    videos_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/ua_detrac_videos/"
+    background_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/ua_detrac_background/"
+    for folder in os.listdir(videos_folder):
+        video_path = videos_folder+folder
+        background_path = background_folder+folder+".jpg"
+        calcBackgound(video_path, 1, background_path)
