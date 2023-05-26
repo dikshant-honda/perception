5dd789349561f0f80764436ab5f926e5abbeb475
dikshant-honda <dikshant_s@jp.honda>
2023-05-26 18:09:57 +0900

    automation added for creating videos

--- code/tests/img_to_mp4.py
+++ code/tests/img_to_mp4.py
@@ -1,34 +1,30 @@
 import os
-from PIL import Image
-import cv2
 import numpy as np
+from PIL import Image, ImageSequence
+import cv2
 
 def create_video(image_folder, video_name):
-    images = [Image.open(f"{image_folder}/{img}") for img in sorted(os.listdir(image_folder)) if img.endswith(".jpg")]
+    images = [f"{image_folder}/{img}" for img in sorted(os.listdir(image_folder)) if img.endswith(".jpg")]
 
-    # Get the dimensions of the first image
-    width, height = images[0].size
+    with Image.open(images[0]) as first_image:
+        width, height = first_image.size
 
-    # Define the video codec
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 
-    # Create a video writer
-    video = cv2.VideoWriter(video_name, fourcc, 25.0, (width, height))
+    video = cv2.VideoWriter(video_name, fourcc, 24.0, (width, height))
 
-    # Iterate over the images and write each frame to the video
-    for image in images:
-        # Convert the PIL image to OpenCV format
-        frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    for image_path in images:
+        with Image.open(image_path) as image:
+            # Convert the PIL image to OpenCV format
+            frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-        # Write the frame to the video
-        video.write(frame)
+            video.write(frame)
 
-    # Release the video writer
     video.release()
 
-# Specify the image folder and video name
-image_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/data/Insight-MVT_Annotation_Train/MVI_20011"
-video_name = "output.mp4"
 
-# Call the function to create the video
-create_video(image_folder, video_name)
+images_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/data/Insight-MVT_Annotation_Train/"
+videos_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/ua_detrac_videos/"
+
+for folder in os.listdir(images_folder):
+    create_video(images_folder+folder, videos_folder+folder+".mp4")
