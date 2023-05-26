0096f31dd5381c5609f31f8751878ff8f99a4d45
dikshant-honda <dikshant_s@jp.honda>
2023-05-26 11:36:35 +0900

    creating video using PIL library

--- code/tests/img_to_mp4.py
+++ code/tests/img_to_mp4.py
@@ -1,59 +1,34 @@
-#!/usr/local/bin/python
-
-import cv2
-import argparse
 import os
+from PIL import Image
+import cv2
+import numpy as np
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-ext", "--extension", required=False, default='png', help="extension name. default is 'png'.")
-ap.add_argument("-o", "--output", required=False, default='output.mp4', help="output video file")
-args = vars(ap.parse_args())
+def create_video(image_folder, video_name):
+    images = [Image.open(f"{image_folder}/{img}") for img in sorted(os.listdir(image_folder)) if img.endswith(".jpg")]
 
-dir_path = '/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/data/Insight-MVT_Annotation_Train/MVI_20035'
-ext = args['extension']
-output = args['output']
+    # Get the dimensions of the first image
+    width, height = images[0].size
 
-images = []
-for f in os.listdir(dir_path):
-    if f.endswith(ext):
-        images.append(f)
+    # Define the video codec
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
 
-image_path = os.path.join(dir_path, images[0])
-frame = cv2.imread(image_path)
-cv2.imshow('video',frame)
-height, width, channels = frame.shape
+    # Create a video writer
+    video = cv2.VideoWriter(video_name, fourcc, 25.0, (width, height))
 
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
-out = cv2.VideoWriter(output, fourcc, 25, (width, height))
+    # Iterate over the images and write each frame to the video
+    for image in images:
+        # Convert the PIL image to OpenCV format
+        frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-for image in images:
+        # Write the frame to the video
+        video.write(frame)
 
-    image_path = os.path.join(dir_path, image)
-    frame = cv2.imread(image_path)
+    # Release the video writer
+    video.release()
 
-    out.write(frame) 
+# Specify the image folder and video name
+image_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/data/Insight-MVT_Annotation_Train/MVI_20011"
+video_name = "output.mp4"
 
-    cv2.imshow('video',frame)
-    if (cv2.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
-        break
-
-out.release()
-cv2.destroyAllWindows()
-
-print("The output video is {}".format(output))
-
-# slowing down the video 
-
-fileName="output.mp4" 
-slomo_frame = int(input("Enter the frames you want to change to \n"))  
-cap = cv2.VideoCapture(fileName)       # load the video
-while(cap.isOpened()):                    # play the video by reading frame by frame
-    ret, frame = cap.read()
-    if ret==True:
-        cv2.imshow('frame',frame)              # show the video
-        if cv2.waitKey(slomo_frame) & 0xFF == ord('q'):
-            break
-    else:
-        break
-cap.release()
-cv2.destroyAllWindows()
+# Call the function to create the video
+create_video(image_folder, video_name)
