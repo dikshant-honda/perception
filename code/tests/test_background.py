761bf5182e7623e2b392424b8b19cc0aaff22e0d
dikshant-honda <dikshant_s@jp.honda>
2023-06-01 17:24:17 +0900

    read full code and try it on sample data

--- code/tests/test_background.py
+++ code/tests/test_background.py
@@ -4,7 +4,7 @@ import numpy as np
 
 ESC_KEY = 27
 
-def checkReduction(image):
+def checkreduction(image):
     reduction = 1
     if image.shape[0]>10000 or image.shape[1]>10000:
         reduction = 10
@@ -16,19 +16,19 @@ def checkReduction(image):
         reduction = 2
     return reduction
 
-def calcBackgound(VideoPath, reduc, Save=None):
+def calcBackground(VideoPath, reduce, Save=None):
     cap = cv2.VideoCapture(VideoPath)
     _, f = cap.read()
-    f= cv2.resize(f, (f.shape[1]// reduc , f.shape[0] // reduc))
+    f= cv2.resize(f, (f.shape[1]// reduce , f.shape[0] // reduce))
     img_bkgd = np.float32(f)
-    reduc = checkReduction(img_bkgd)
+    reduce = checkreduction(img_bkgd)
     while True:
         ret, f = cap.read()
         if not ret: break
-        cv2.imshow('Main Video', cv2.resize(f, (f.shape[1]// reduc , f.shape[0] // reduc)))
+        cv2.imshow('Main Video', cv2.resize(f, (f.shape[1]// reduce , f.shape[0] // reduce)))
         cv2.accumulateWeighted(f, img_bkgd, 0.01)
         res2 = cv2.convertScaleAbs(img_bkgd)
-        cv2.imshow('When you feel the background is good enough, press ESC to terminate and save the background.', cv2.resize(res2, (res2.shape[1]// reduc , res2.shape[0] // reduc)))
+        cv2.imshow('When you feel the background is good enough, press ESC to terminate and save the background.', cv2.resize(res2, (res2.shape[1]// reduce , res2.shape[0] // reduce)))
         k = cv2.waitKey(20)
         if k == 27: break
     if Save: cv2.imwrite(Save, res2)
@@ -39,8 +39,11 @@ def calcBackgound(VideoPath, reduc, Save=None):
 if __name__ == "__main__":
     videos_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/ua_detrac_videos/"
     background_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/ua_detrac_background/"
-    for folder in os.listdir(videos_folder):
-        print("removing background from:", folder)
-        video_path = videos_folder+folder
-        background_path = background_folder+folder+".jpg"
-        calcBackgound(video_path, 1, background_path)
+    video_file = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/test1.mp4"
+    background_file = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/background.jpg"
+    # for folder in os.listdir(videos_folder):
+    #     print("removing background from:", folder)
+    #     video_path = videos_folder+folder
+    #     background_path = background_folder+folder+".jpg"
+    #     calcBackground(video_path, 1, background_path)
+    calcBackground(video_file, 1, background_file)
