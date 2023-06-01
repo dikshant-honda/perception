74345cb0365b5b8e52271b8b19c17919879d9ccc
dikshant-honda <dikshant_s@jp.honda>
2023-06-01 17:33:11 +0900

    second background image ready

--- code/tests/test_background.py
+++ code/tests/test_background.py
@@ -39,8 +39,8 @@ def calcBackground(VideoPath, reduce, Save=None):
 if __name__ == "__main__":
     videos_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/ua_detrac_videos/"
     background_folder = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/ua_detrac_background/"
-    video_file = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/data/test1.mp4"
-    background_file = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/background_1.jpg"
+    video_file = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/data/test2.mp4"
+    background_file = "/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/background_2.jpg"
     # for folder in os.listdir(videos_folder):
     #     print("removing background from:", folder)
     #     video_path = videos_folder+folder
