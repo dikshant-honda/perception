62aafb6cf51dd8f336551a4caa74750f478fb8fd
dikshant-honda <dikshant_s@jp.honda>
2023-06-07 18:39:52 +0900

    road border ready

--- code/tests/test_road_edges.py
+++ code/tests/test_road_edges.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/ua_detrac_background/MVI_40141.mp4.jpg")
+image = cv2.imread("/home/dikshant/3D-Net-Monocular-3D-Object-Recognition-for-Traffic-Monitoring/code/tests/Road mask.png")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-edges = cv2.Canny(blurred, 150, 200, apertureSize = 3)  
+edges = cv2.Canny(blurred, 50, 100)  
 
 height, width = edges.shape[:2]
-roi_vertices = np.array([[(0, height), (width/2, 0), (width, height)]], dtype=np.int32)
+roi_vertices = np.array([[(0, height), (0, 0), (width, 0),(width, height)]], dtype=np.int32)
 
 mask = np.zeros_like(edges)
 
@@ -20,5 +20,6 @@ masked_edges = cv2.bitwise_and(edges, mask)
 # Extract the road edges
 road_edges = masked_edges
 
+cv2.imwrite("Road Border.bmp", road_edges)
 cv2.imshow("Road Edges", road_edges)
 cv2.waitKey(0)
